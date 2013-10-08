bdf58ab7282bb71f40df1b3a8c113061cd89f7db
Haiping Xia <haxia@deloitte.com>
2013-10-08 15:27:04 -0400

    origianl i2b2 1.6.09 source codes from i2b2.org

---
+++ Server/admin/i2b2_config_data.js
@@ -0,0 +1,16 @@
+{
+	urlProxy: "index.php",
+	urlFramework: "js-i2b2/",
+	//-------------------------------------------------------------------------------------------
+	// THESE ARE ALL THE DOMAINS A USER CAN LOGIN TO
+	lstDomains: [
+		{ domain: "HarvardDemo",
+		  name: "HarvardDemo",
+		  urlCellPM: "http://webservices.i2b2.org/i2b2/rest/PMService/",
+		  allowAnalysis: true,
+		  adminOnly: true,
+		  debug: false
+		}
+	]
+	//-------------------------------------------------------------------------------------------
+}
